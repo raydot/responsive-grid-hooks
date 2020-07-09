3d1f95b66466ebeb097a8a51a8fd4986a0275b51
Dave Kanter <moatgoat@hotmail.com>
2020-07-09 13:51:30 -0700

    First and last commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import PplCard from './PplCard';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      setUsers(
+        await fetch('https://reqres.in/api/users')
+          .then((res) => res.json())
+          .then((res) => res.data)
+      );
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Foo();
+      <Grid container spacing={10} style={{ padding: '24px' }}>
+        {users.map((users) => (
+          <Grid key={users.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+            <PplCard
+              key={users.id}
+              email={users.email}
+              firstname={users.first_name}
+              lastname={users.last_name}
+              avatar={users.avatar}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
